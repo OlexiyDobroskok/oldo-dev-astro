d0da573ec9359e4c5c6be83dc2dd5f62f737955b
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-09-08 00:44:51 +0300

    Configure tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,tsx}'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"SUSE"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+      },
+      gridTemplateRows: {
+        0: '0fr',
+        1: '1fr',
+      },
+      transitionProperty: {
+        'grid-rows': 'grid-template-rows, margin, padding',
+      },
+    },
+  },
+  plugins: [],
+};
