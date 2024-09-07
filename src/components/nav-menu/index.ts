8bf3a8c7d9b4d6155478e1b6c6204e7771ff3201
Oleksii Dobroskok <a.v.dobroskok.kh@gmail.com>
2024-09-08 00:50:56 +0300

    Initial Desktop nav menu

---
+++ src/components/nav-menu/index.ts
@@ -0,0 +1,4 @@
+export { default as DesktopNavMenu } from './desktop/desktop-nav-menu.astro';
+export { default as MobileNavMenu } from './mobile/mobile-nav-menu.astro';
+
+export type { MenuLink } from './types';
